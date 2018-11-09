4c086ebd1d7fe10750d89b3234889f4f87f4f81d
Victoria <victoria.milkova@gmail.com>
2018-11-09 02:36:33 +0200

    subtab-AdminCatalog excluded from test because it fails to get submenu ID refactoring read Chrome driver location from System Env variables

--- src/test/java/com/Prestashop/InputLoginAndPassword.java
+++ src/test/java/com/Prestashop/InputLoginAndPassword.java
@@ -10,21 +10,15 @@ public class InputLoginAndPassword extends Settings {
 
     private String[] subMenu = {
             "subtab-AdminParentOrders",//1
-            "subtab-AdminCatalog",//2
+//            "subtab-AdminCatalog",//2
             "subtab-AdminParentCustomer",//3
             "subtab-AdminParentCustomerThreads",//4
             "subtab-AdminStats"//5
     };
 
-    //    @Test
+    @Test
     public void scriptA() {
-
-        driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
-
-        driver.findElementById("email").sendKeys("webinar.test@gmail.com");
-        driver.findElementById("passwd").sendKeys("Xcg7299bnSmMuRLp9ITw");
-        driver.findElementByName("submitLogin").click();
-
+        login();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.findElementById("employee_infos").click();
         driver.findElementById("header_logout").click();
@@ -32,23 +26,23 @@ public class InputLoginAndPassword extends Settings {
 
     @Test
     public void scriptB() throws InterruptedException {
-
-        driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
-        driver.findElementById("email").sendKeys("webinar.test@gmail.com");
-        driver.findElementById("passwd").sendKeys("Xcg7299bnSmMuRLp9ITw");
-        driver.findElementByName("submitLogin").click();
-        driver.findElementById("tab-AdminDashboard").click();
-
+        login();
         Actions actions = new Actions(driver);
         for (String subMenuItem : subMenu) {
             WebElement element = driver.findElementById(subMenuItem);
             actions.moveToElement(element).click().build().perform();
-            driver.manage().timeouts().implicitlyWait(20, TimeUnit.MINUTES);
-//            Thread.sleep(5000);
+            driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
             String firstLoadChapterTitle = driver.findElementByClassName("page-title").getText();
             driver.navigate().refresh();
             String secondLoadChapterTitle = driver.findElementByClassName("page-title").getText();
-            System.out.println("Title is correct: " + (firstLoadChapterTitle.equals(secondLoadChapterTitle)));
+            System.out.println("Chapter " + secondLoadChapterTitle + " is correct: " + (firstLoadChapterTitle.equals(secondLoadChapterTitle)));
         }
+    }
+
+    private void login() {
+        driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
+        driver.findElementById("email").sendKeys("webinar.test@gmail.com");
+        driver.findElementById("passwd").sendKeys("Xcg7299bnSmMuRLp9ITw");
+        driver.findElementByName("submitLogin").click();
     }
 }
--- src/test/java/com/Prestashop/Settings.java
+++ src/test/java/com/Prestashop/Settings.java
@@ -1,11 +1,8 @@
 package com.Prestashop;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-
 import java.util.concurrent.TimeUnit;
 
 public class Settings {
@@ -13,21 +10,14 @@ public class Settings {
     public ChromeDriver driver;
 
     @BeforeTest
-
     public void setUp() {
-
-        System.setProperty("webdriver.chrome.driver", "/home/victoria/Загрузки/chromedriver");
+        System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER"));
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-
-
-
     }
 
     @AfterTest
-
     public void close(){
     driver.quit();
     }
